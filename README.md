5b68332ee629256875d4d2b7a0357199acbdcddb
Daniel <daniel.holbling@gmail.com>
2019-11-06 13:32:50 +0100

    Added quote function to landing page

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Slutprojekt JavaScript
 
 ## Description
+Portfolio page that does stuff with javascript.
+
 
 ## Features
+Landing page fetches quotes from quotable API.
+
 
 ## Libraries
+Probably going to use animejs
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,45 @@
 import '../styles/index.scss';
+const contentWrapper = document.querySelector('.content-wrapper');
 
-console.log('webpack starterkit');
+async function jsonThisRequestURL(url){
+    let req = await fetch('https://api.quotable.io/random');
+    let myJson = req.json();
+    return myJson;
+}
+
+
+
+
+
+class Quote {
+    constructor() {
+        let myJson = jsonThisRequestURL('https://api.quotable.io/random');
+        this.quote = myJson.quote;
+        this.author = myJson.author;
+    }
+    
+    async updateQuote() {
+        let json = await jsonThisRequestURL('https://api.quotable.io/random');
+        this.quote = json.content;
+        this.author = json.author;
+    }
+
+    markup() {
+        return `
+        <div class="quote-wrapper">
+            <p class="quote">${this.quote}</p>
+            <p class="author">${this.author}</p>
+        </div>
+        `;
+    }
+}
+
+
+
+async function init(){
+    let newQuote = new Quote();
+    await newQuote.updateQuote();
+    contentWrapper.innerHTML = newQuote.markup();
+}
+
+init();
