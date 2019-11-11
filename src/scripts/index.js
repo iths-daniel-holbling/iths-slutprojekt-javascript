046cca1fbaf939649ddc642003e1a0a4831794d7
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2019-11-11 16:34:14 +0100

    Building madlibs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -62,15 +62,33 @@ const animate = {
     }
 };
 
-class Madlib {
-    constructor(blanks,story) {
-        this.numberOfBlanks = blanks.length;
-        this.blankNames = blanks;
-        this.story =  story;
+let madlib = {
+    blankNames: ['adjective','adjective','type of bird','room in a house','verb (past tense)','verb','relatives name','noun','a liquid','verb ending in -ing','part of the body (plural preferred)','plural noun','verb ending in -ing','noun'],
+    blanks: [],
+    inputRender(){
+        contentWrapper.innerHTML = "";
+        let wrapper = document.createElement('div');
+        wrapper.className = "input-wrapper";
+        contentWrapper.appendChild(wrapper);
+        for(let blankName of this.blankNames){
+            let input = document.createElement('input');
+            input.setAttribute("type","text");
+            input.setAttribute("placeholder",blankName);
+            input.setAttribute("data-aos","fade-up");
+            wrapper.appendChild(input);
+        }
+        let submit = document.createElement('button');
+        submit.innerText = "Create";
+        wrapper.appendChild(submit);
+    },
+    storyMarkup(){
+        return `
+        It was a ${this.blanks[0]}, cold November day. I woke up to the ${this.blanks[1]} smell of ${this.blanks[2]} roasting in the ${this.blanks[3]} downstairs. I ${this.blanks[4]} down the stairs to see if I could help ${this.blanks[5]} the dinner. My mom said, "See if ${this.blanks[6]} needs a fresh ${this.blanks[7]}." So I carried a tray of glasses full of ${this.blanks[8]} into the ${this.blanks[9]} room. When I got there, I couldn't believe my ${this.blanks[10]}! There were ${this.blanks[11]} ${this.blanks[12]} on the ${this.blanks[13]}!
+        `;
     }
 
 
-}
+};
 
 function initEventListeners(){
     let home = document.querySelector('#home');
@@ -82,9 +100,11 @@ function initEventListeners(){
     });
     madlibs.addEventListener('click', () => {
         animate.out(".content-wrapper");
+        madlib.inputRender();
+        animate.in(".content-wrapper");
     });
-
 }
 
+console.log(madlib);
 newQuote();
 initEventListeners();
