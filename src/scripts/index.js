4afd9291b89996723287de19b89e5c706dc08a9d
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2019-11-11 14:30:08 +0100

    Added animated reload of landing page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,7 +8,7 @@ AOS.init();
 const contentWrapper = document.querySelector('.content-wrapper');
 
 async function jsonThisRequestURL(url){
-    let req = await fetch('https://api.quotable.io/random');
+    let req = await fetch(url);
     let myJson = req.json();
     return myJson;
 }
@@ -28,21 +28,49 @@ class Quote {
     markup() {
         return `
         <div class="quote-wrapper">
-            <p class="quote" data-aos="fade-left">${this.quote}</p>
-            <p class="author">- <em>${this.author}</em></p>
+            <p class="quote" data-aos="fade-left" data-aos-delay=500>${this.quote}</p>
+            <p class="author" data-aos="fade-right" data-aos-delay=800>- <em>${this.author}</em></p>
         </div>
         `;
     }
 }
 
+const animate = {
+    pageOut(targets){
+        anime({
+            targets: targets,
+            translateX: 250,
+            opacity: 0
+        });
+    },
+    pageIn(targets){
+        anime({
+            targets: targets,
+            translateX: 0,
+            opacity: 1
+        });
+    }
+};
 
+function initEventListeners(){
+    let home = document.querySelector('#home');
+    home.addEventListener('click', async () => {
+        // animate.pageOut(contentWrapper);
+        animate.pageOut(".content-wrapper");
+        await newQuote();
+        animate.pageIn(contentWrapper);
+    });
+}
 
-async function init(){
+async function newQuote(){
     let newQuote = new Quote();
     await newQuote.updateQuote();
     if(newQuote.author !== "Donald Trump"){
         contentWrapper.innerHTML = newQuote.markup();
+    }else{
+        newQuote();
     }
 }
 
-init();
+newQuote();
+initEventListeners();
